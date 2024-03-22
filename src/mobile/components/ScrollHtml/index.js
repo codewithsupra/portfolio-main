8e2caebb3a6eea83ef8cb22f07aa720e5b459c90
SUPRATIM SARKAR <46316649+codewithsupra@users.noreply.github.com>
2024-03-22 12:03:08 +0400

    Update index.js

--- src/mobile/components/ScrollHtml/index.js
+++ src/mobile/components/ScrollHtml/index.js
@@ -1,26 +1,22 @@
-import { Scroll, useProgress, useScroll } from "@react-three/drei";
-import React, { forwardRef, useState } from "react";
+import { Scroll, useProgress } from "@react-three/drei";
+import React, { useState } from "react";
 import { WelcomeComponent } from "./WelcomeComponent";
-import { NavigationHologram } from "../../models/Navigation/NaviagtionHologram/NavigationHologram";
-import { useFrame } from "@react-three/fiber";
-import { ProfileText } from "./ProfileText";
 import { NavigationText } from "./NavigationText";
+import { ProfileText } from "./ProfileText";
 import { SkillText } from "./SkillText";
 import { ProjectText } from "./ProjectText";
 
 export const ScrollHtml = () => {
   const { progress } = useProgress();
-  const scroll = useScroll();
-  const [welcomeOpacity, setWelcomeOpactity] = useState(1);
-  const [navigationOpacity, setNavigationOpactity] = useState(1);
-  const [profileOpacity, setProfileOpactity] = useState(1);
-  const [skillOpacity, setSkillOpactity] = useState(1);
+  const [welcomeOpacity, setWelcomeOpacity] = useState(1);
+  const [navigationOpacity, setNavigationOpacity] = useState(1);
+  const [profileOpacity, setProfileOpacity] = useState(1);
+  const [skillOpacity, setSkillOpacity] = useState(1);
 
-  useFrame(() => {
-    setWelcomeOpactity(1 - scroll.range(0, 1 / 10));
-    setNavigationOpactity(1 - scroll.range(0, 1 / 10));
-    setProfileOpactity(1 - scroll.range(4.1 / 10, 1 / 10));
-  });
+  const updateOpacity = (rangeStart, rangeEnd, setter) => {
+    const opacity = 1 - progress.range(rangeStart, rangeEnd);
+    setter(opacity);
+  };
 
   return (
     <>
